bb5b00baaeefeb62f30a55636efe53bf9ef4104b
Khánh Phùng <91212840+pmk2k1@users.noreply.github.com>
2024-03-02 04:07:23 +0700

    add function to increase and decrease quantity

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,46 +15,47 @@ class CartController < ApplicationController
       @cart.orderables.create(product: @product, quantity:)
     end
 
-    redirect_to root_path
+    # redirect_to root_path
     # respond_to do |format|
     #   format.turbo_stream do
     #     render turbo_stream: [turbo_stream.replace('cart',
     #                                                partial: 'cart/orderables',
     #                                                locals: { cart: @cart }),
-    #                           turbo_stream.replace(@product)]
+    #                           turbo_stream.replace('cart/orderables')]
     #   end
     # end
+    redirect_to cart_add_path
   end
 
   def add_quantity
-    # @product = Product.find_by(id: params[:id])
-    # quantity = params[:quantity].to_i
-    current_orderable = @cart.orderables.find(params[:id])
+    @product = Product.find_by(id: params[:id])
+    quantity = params[:quantity].to_i
+
+    current_orderable = @cart.orderables.find_by(params[product_id: @product])
     current_orderable.quantity += 1
-    # current_orderable.save
-    current_orderable.update(quantity:)
-    redirect_to root_path
+    current_orderable.save
+    # current_orderable.update(quantity:)
+    render partial: 'cart/cart'
   end
 
   def reduce_quantity
     @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
-    current_orderable = @cart.orderables.find_by(product_id: @product.id)
+    current_orderable = @cart.orderables.find_by(params[:id])
     if current_orderable.quantity > 1
       current_orderable.quantity -= 1
-      current_orderable.update(quantity:)
+      current_orderable.save
     else
       current_orderable.destroy
     end
 
-    # current_orderable.save
-    redirect_to root_path
+    render partial: 'cart/cart'
   end
 
   def remove
     @cart_details = Orderable.find(params[:id])
     @cart_details.destroy
-    redirect_to root_path
+    # redirect_to root_path
     # respond_to do |format|
     #   format.turbo_stream do
     #     render turbo_stream: turbo_stream.replace('cart',
@@ -62,5 +63,6 @@ class CartController < ApplicationController
     #                                               locals: { cart: @cart })
     #   end
     # end
+    render partial: 'cart/cart'
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,108 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 
-document.getElementsByClassName("addcart").onclick = function() {
-  document.getElementsByClassName("addcart").innerHTML = "AVAILABLE IN CART";
+var cart_2 = document.getElementById("cart-2");
+if(cart_2.id == "cart-2") {
+  document.getElementById("button-cart-2").value = "AVAILABLE IN CART";
+}
+var cart_3 = document.getElementById("cart-3");
+if(cart_3.id == "cart-3") {
+  document.getElementById("button-cart-3").value = "AVAILABLE IN CART";
+}
+var cart_4 = document.getElementById("cart-4");
+if(cart_4.id == "cart-4") {
+  document.getElementById("button-cart-4").value = "AVAILABLE IN CART";
+}
+var cart_5 = document.getElementById("cart-5");
+if(cart_5.id == "cart-5") {
+  document.getElementById("button-cart-5").value = "AVAILABLE IN CART";
+}
+var cart_6 = document.getElementById("cart-6");
+if(cart_6.id == "cart-6") {
+  document.getElementById("button-cart-6").value = "AVAILABLE IN CART";
+}
+var cart_7 = document.getElementById("cart-7");
+if(cart_7.id == "cart-7") {
+  document.getElementById("button-cart-7").value = "AVAILABLE IN CART";
+}
+var cart_8 = document.getElementById("cart-8");
+if(cart_8.id == "cart-8") {
+  document.getElementById("button-cart-8").value = "AVAILABLE IN CART";
+}
+var cart_9 = document.getElementById("cart-9");
+if(cart_9.id == "cart-9") {
+  document.getElementById("button-cart-9").value = "AVAILABLE IN CART";
+}
+var cart_10 = document.getElementById("cart-10");
+if(cart_10.id == "cart-10") {
+  document.getElementById("button-cart-10").value = "AVAILABLE IN CART";
+}
+var cart_11 = document.getElementById("cart-11");
+if(cart_11.id == "cart-11") {
+  document.getElementById("button-cart-11").value = "AVAILABLE IN CART";
+}
+
+//-------------------------------------------------------------
+
+document.getElementById("button-cart-2").onclick = function() {
+  document.getElementById("button-cart-2").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-3").onclick = function() {
+  document.getElementById("button-cart-3").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-4").onclick = function() {
+  document.getElementById("button-cart-4").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-5").onclick = function() {
+  document.getElementById("button-cart-5").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-6").onclick = function() {
+  document.getElementById("button-cart-6").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-7").onclick = function() {
+  document.getElementById("button-cart-7").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-8").onclick = function() {
+  document.getElementById("button-cart-8").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-9").onclick = function() {
+  document.getElementById("button-cart-9").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-10").onclick = function() {
+  document.getElementById("button-cart-10").value = "AVAILABLE IN CART";
+};
+document.getElementById("button-cart-11").onclick = function() {
+  document.getElementById("button-cart-11").value = "AVAILABLE IN CART";
+};
+//-------------------------------------------------------
+document.getElementById("cart-2").onclick = function() {
+  document.getElementById("button-cart-2").value = "ADD TO CART";
+};
+document.getElementById("cart-3").onclick = function() {
+  document.getElementById("button-cart-3").value = "ADD TO CART";
+};
+document.getElementById("cart-4").onclick = function() {
+  document.getElementById("button-cart-4").value = "ADD TO CART";
+};
+document.getElementById("cart-5").onclick = function() {
+  document.getElementById("button-cart-5").value = "ADD TO CART";
+};
+document.getElementById("cart-6").onclick = function() {
+  document.getElementById("button-cart-6").value = "ADD TO CART";
+};
+document.getElementById("cart-7").onclick = function() {
+  document.getElementById("button-cart-7").value = "ADD TO CART";
+};
+document.getElementById("cart-8").onclick = function() {
+  document.getElementById("button-cart-8").value = "ADD TO CART";
+};
+document.getElementById("cart-9").onclick = function() {
+  document.getElementById("button-cart-9").value = "ADD TO CART";
+};
+document.getElementById("cart-10").onclick = function() {
+  document.getElementById("button-cart-10").value = "ADD TO CART";
+};
+document.getElementById("cart-11").onclick = function() {
+  document.getElementById("button-cart-11").value = "ADD TO CART";
 };
 
-// $(document).ready(function () {
-//   $(".addcart").click(function () {
-//     value = "AVAILABLE IN CART";
-//   });
-// });

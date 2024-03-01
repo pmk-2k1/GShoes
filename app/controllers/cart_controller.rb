1cd4714fc773684eed2e39b2b4aac9647ff800ce
Khánh Phùng <91212840+pmk2k1@users.noreply.github.com>
2024-03-02 05:27:28 +0700

    Edit the function to increase and decrease the quantity in the shopping cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -14,55 +14,32 @@ class CartController < ApplicationController
     else
       @cart.orderables.create(product: @product, quantity:)
     end
-
-    # redirect_to root_path
-    # respond_to do |format|
-    #   format.turbo_stream do
-    #     render turbo_stream: [turbo_stream.replace('cart',
-    #                                                partial: 'cart/orderables',
-    #                                                locals: { cart: @cart }),
-    #                           turbo_stream.replace('cart/orderables')]
-    #   end
-    # end
     redirect_to cart_add_path
   end
 
   def add_quantity
-    @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
-
-    current_orderable = @cart.orderables.find_by(params[product_id: @product])
+    current_orderable = Orderable.find(params[:id])
     current_orderable.quantity += 1
-    current_orderable.save
-    # current_orderable.update(quantity:)
+    current_orderable.save(quantity:)
     render partial: 'cart/cart'
   end
 
   def reduce_quantity
-    @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
-    current_orderable = @cart.orderables.find_by(params[:id])
+    current_orderable = Orderable.find(params[:id])
     if current_orderable.quantity > 1
       current_orderable.quantity -= 1
-      current_orderable.save
+      current_orderable.save(quantity:)
     else
       current_orderable.destroy
     end
-
     render partial: 'cart/cart'
   end
 
   def remove
     @cart_details = Orderable.find(params[:id])
     @cart_details.destroy
-    # redirect_to root_path
-    # respond_to do |format|
-    #   format.turbo_stream do
-    #     render turbo_stream: turbo_stream.replace('cart',
-    #                                               partial: 'cart/orderables',
-    #                                               locals: { cart: @cart })
-    #   end
-    # end
     render partial: 'cart/cart'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   get 'cart', to: 'cart#show'
+
   post 'cart/add'
-  post 'cart/add_quantity'
-  post 'cart/reduce_quantity'
+
+  post 'cart/:id/add_quantity' => 'cart#add_quantity', as: 'cart_add_quantity'
+
+  post 'cart/:id/reduce_quantity' => 'cart#reduce_quantity', as: 'cart_reduce_quantity'
+
   post 'cart/remove'
+
   resources :products
+
   root 'products#index'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
